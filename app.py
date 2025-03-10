cd465f85959f6b957082503aebd71f730b4a0f68
osamakhurma <osamakhurma98@gmail.com>
2025-03-10 21:00:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import random
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.')
 
 # البيانات الخاصة بالمحافظات والمدن (مخزنة محليًا)
 provinces = [
